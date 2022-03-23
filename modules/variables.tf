112f2fce9b2703663b1592d38a7312fe681703b9
SurendraReddyWesco <98414596+SurendraReddyWesco@users.noreply.github.com>
2022-03-23 15:18:06 +0530

    Update variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,6 +1,6 @@
 variable "cluster_name" {
   description = "The name for the AKS cluster"
-  default     = "learnk8scluster"
+  default     = "basick8scluster"
 }
 variable "env_name" {
   description = "The environment for the AKS cluster"
